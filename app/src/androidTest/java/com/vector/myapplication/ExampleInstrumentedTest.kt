ff5971c9848ee1158d823c2415ef62c6e0ed3339
vectorqi <qeemiaomiao@gmail.com>
2025-01-15 16:54:23 -0800

    Fix Instrumented Test error

--- app/src/androidTest/java/com/vector/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/vector/myapplication/ExampleInstrumentedTest.kt
@@ -3,14 +3,10 @@ package com.vector.myapplication
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -24,6 +20,6 @@ class ExampleInstrumentedTest {
 
     @Test
     fun testGreetingText() {
-        composeTestRule.onNodeWithText("Hello Android").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Hello Android!").assertIsDisplayed()
     }
 }
