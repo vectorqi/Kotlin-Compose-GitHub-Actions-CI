33994639c3ce30832267dd94ac70b6eff32f272e
vectorqi <qeemiaomiao@gmail.com>
2025-01-16 17:08:14 -0800

    ComposeActivity Finished

--- app/src/main/java/com/vector/myapplication/TestComposeActivity.kt
+++ app/src/main/java/com/vector/myapplication/TestComposeActivity.kt
@@ -1,10 +1,14 @@
 package com.vector.myapplication
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,45 +16,101 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.vector.myapplication.ui.theme.KotlinComposeGitHubActionsCITheme
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 
 class TestComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MessageCard(msg = Message("Vector", "Jetpack Compose"))
+            KotlinComposeGitHubActionsCITheme {
+                MessageCard(msg = Message("Vector", "Jetpack Compose"))
+            }
         }
     }
 }
 data class Message (val author: String, val body: String)
 
 @Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)){
-        Image(painter = painterResource(R.drawable.profile_pic),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier.size(40.dp).clip(CircleShape)
-            )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column{
-            Text(text = msg.author)
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+fun Conversation(messages : List<Message>) {
+    LazyColumn {
+        items(messages){ message ->
+            MessageCard(message)
         }
     }
-
 }
 
-@Preview(showBackground = true)
+@Preview
+@Composable
+fun PreviewConversation() {
+    KotlinComposeGitHubActionsCITheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Composable
+fun MessageCard(msg: Message) {
+    KotlinComposeGitHubActionsCITheme {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.profile_pic),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier.size(40.dp).clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                    .align(Alignment.CenterVertically)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            var isExpanded by remember{mutableStateOf(false)}
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            )
+
+            Column(modifier = Modifier.clickable {isExpanded = !isExpanded}) {
+                Text(text = msg.author,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall)
+                Spacer(modifier = Modifier.height(4.dp))
+                Surface(shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier.animateContentSize().padding(1.dp)) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+    }
+}
+@Preview(name = "Light Mode")
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
 @Composable
 fun MessageCardPreview() {
-    MessageCard(msg = Message("Tracy", "Take a look at the Jetpack Compose"))
+    KotlinComposeGitHubActionsCITheme {
+        Surface {
+            MessageCard(msg = Message("Tracy", "Take a look at the Jetpack Compose"))
+        }
+    }
 }
