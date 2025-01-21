715b0ca05efac96b3ec204e96a3103215bda3607
vectorqi <qeemiaomiao@gmail.com>
2025-01-21 14:39:55 -0800

    Main Activity Implementation - Lazy Column

--- app/src/main/java/com/vector/myapplication/MainActivity.kt
+++ app/src/main/java/com/vector/myapplication/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +21,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -60,11 +62,10 @@ fun MyAppPreview(){
 
 @Composable
 fun Greetings(modifier: Modifier = Modifier,
-              names: List<String> = listOf("WakakaLlala","Huhaha")) {
-
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for(name in names){
-            Greeting(name)
+              names: List<String> = List(1000){"$it"}) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+       items (items = names) {name ->
+            Greeting(name = name)
         }
     }
 }
